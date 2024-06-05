d1ce7e3d5be472f31388c30594785dfdc1bb25d2
LORAN <vinana222@gmail.com>
2024-06-05 22:51:31 +0300

    added submodule raylib and raygui

--- FarishEngineEditor/src/main.cpp
+++ FarishEngineEditor/src/main.cpp
@@ -1,12 +1,25 @@
-#include <iostream>
-#include "FarishEngineCore\test.h"
-
-using namespace std;
+#include <raylib.h>
 
 int main()
 {
-	cout << "Farish make files" << endl;
+	const int screenWidth = 800;
+	const int screenHeight = 600;
 
-	Test test;
-	test.update();
+	InitWindow(screenWidth, screenHeight, "Farish Engine");
+
+	SetWindowState(FLAG_WINDOW_RESIZABLE);
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(SKYBLUE);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return 0;
 }
